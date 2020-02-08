ef199c4cc7ae93de3cbd595e0d7c7656f4748a4b
lijainhao-ljh <122572191@qq,com>
2020-02-09 01:04:22 +0800

    111

--- project/前台代码/德召文车辆维修管理系统/js/my.js
+++ project/前台代码/德召文车辆维修管理系统/js/my.js
@@ -25,19 +25,26 @@ function loadPerms(){
 }
 //loadPerms();
 
-function openIFM(item){
-	var flag = $(".my-tab>li>a[href='#"+item.ename+"']");
-	$(".my-tab>li").removeClass("active");
-	$(".tab-content>div").removeClass("active");
-	if(flag[0]) {
-		flag.parent().addClass("active");
-		$(".tab-content>div#"+item.ename).addClass("active");
-		return;
-	};
-	let nav_li = `<li class="active"><a href="#${item.ename}" data-toggle="tab">${item.name}</a></li>`;
-	let tab_div = `<div id="${item.ename}" class="active tab-pane">
-					<iframe src="./html/${item.path}"></iframe>
-				</div>`;
-	$(".my-tab").append(nav_li);
-	$(".tab-content").append(tab_div);
+function openIFM(it){
+	if(it==0){
+							$(".my-tab>li").removeClass("active");
+							$(".tab-content>div").removeClass("active");
+							$("[href='#index']").parent().addClass("active");
+							$("#index").addClass("active");
+							return;
+						}
+						var flag = $(".my-tab>li>a[href='#"+it.cename+"']");
+						$(".my-tab>li").removeClass("active");
+						$(".tab-content>div").removeClass("active");
+						if(flag[0]) {
+							flag.parent().addClass("active");
+							$(".tab-content>div#"+it.cename).addClass("active");
+							return;
+						};
+						let nav_li = `<li class="active"><a href="#${it.cename}" data-toggle="tab">${it.name}</a></li>`;
+						let tab_div = `<div id="${it.cename}" style="height: 100%;" class="active tab-pane">
+									<iframe src="${it.path}"></iframe>
+								</div>`;
+						$(".my-tab").append(nav_li);
+						$(".tab-content").append(tab_div);
 }
