35d71492df67aa35bca59610d0b926d71f4b9dc9
lijainhao-ljh <122572191@qq,com>
2020-02-08 14:36:35 +0800

    aaa

--- project/bin/前台代码/德召文车辆维修管理系统/js/my.js
+++ project/bin/前台代码/德召文车辆维修管理系统/js/my.js
@@ -1,5 +1,43 @@
-
-
+//全局参数定义
 var data = {
-	ip:"http://127.0.0.1:8080/"
+	ip:"http://localhost:8080/",
+	perms:[],
+	params:{}
+}
+
+//加载权限内容.
+function loadPerms(){
+	$.ajax({
+		url: data.ip + "perms/findPerms",
+		type: "get",
+		xhrFields: {
+			withCredentials: true
+		},
+		dataType: "json",
+		success: function(response) {
+			console.log(response)
+			data.perms = response;
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	});
+}
+//loadPerms();
+
+function openIFM(item){
+	var flag = $(".my-tab>li>a[href='#"+item.ename+"']");
+	$(".my-tab>li").removeClass("active");
+	$(".tab-content>div").removeClass("active");
+	if(flag[0]) {
+		flag.parent().addClass("active");
+		$(".tab-content>div#"+item.ename).addClass("active");
+		return;
+	};
+	let nav_li = `<li class="active"><a href="#${item.ename}" data-toggle="tab">${item.name}</a></li>`;
+	let tab_div = `<div id="${item.ename}" class="active tab-pane">
+					<iframe src="./html/${item.path}"></iframe>
+				</div>`;
+	$(".my-tab").append(nav_li);
+	$(".tab-content").append(tab_div);
 }
