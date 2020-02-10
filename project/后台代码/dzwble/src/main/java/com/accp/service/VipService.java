152fc22007e2525053376f233a6f2595184af19a
lijainhao-ljh <122572191@qq,com>
2020-02-10 16:29:32 +0800

    1111

---
+++ project/后台代码/dzwble/src/main/java/com/accp/service/VipService.java
@@ -0,0 +1,43 @@
+package com.accp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.accp.mapper.VipMapper;
+
+@Service
+@Transactional
+public class VipService {
+	
+	@Autowired
+	VipMapper r;
+	
+	/*
+	 * 查询会员
+	 */
+	
+	/*
+	 * 新增会员
+	 */
+
+	/*
+	 * 根据编号查询会员
+	 */
+	
+	/*
+	 * 修改会员信息
+	 */
+	
+	/*
+	 * 删除会员
+	 */
+	
+	/*
+	 * 积分清零
+	 */
+	
+	/*
+	 * 会员充值
+	 */
+}
