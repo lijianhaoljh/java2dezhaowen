040f735311dbf7386953756a664ce71e29998cd0
zhongpenggen <871776974@qq.com>
2020-02-10 08:48:16 +0800

    11

--- project/后台代码/dzwble/src/main/java/com/accp/controller/ZCommodityInfoController.java
+++ project/后台代码/dzwble/src/main/java/com/accp/controller/ZCommodityInfoController.java
@@ -7,7 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.accp.domain.CommodityBrand;
 import com.accp.domain.CommodityInfo;
+import com.accp.domain.CommodityUnit;
+import com.accp.domain.CountryOrigin;
+import com.accp.domain.GoodsCategory;
+import com.accp.domain.GradeCommodity;
+import com.accp.domain.IncomeType;
+import com.accp.domain.Manufacturer;
+import com.accp.domain.PlaceOrigin;
 import com.accp.service.ZCommodityInfoService;
 
 @RestController
@@ -15,6 +23,41 @@ import com.accp.service.ZCommodityInfoService;
 public class ZCommodityInfoController {
 			@Autowired
 			ZCommodityInfoService zCommodityInfoService;
+			
+			@GetMapping(value = "query")
+			public List<CommodityBrand> query(){
+				return zCommodityInfoService.query();
+			}
+			@GetMapping(value = "querya")
+			public List<CommodityUnit> querya(){
+				return zCommodityInfoService.querya();
+			}
+			@GetMapping(value = "queryab")
+			public List<GoodsCategory> queryab(){
+				return zCommodityInfoService.queryab();
+			}
+			@GetMapping(value = "queryac")
+			public List<IncomeType> queryac(){
+				return zCommodityInfoService.queryac();
+			}
+			@GetMapping(value = "queryad")
+			public List<CountryOrigin> queryad(){
+				return zCommodityInfoService.queryad();
+			}
+			@GetMapping(value = "querye")
+			public List<GradeCommodity> querye(){
+				return zCommodityInfoService.querye();
+			}
+			@GetMapping(value = "queryef")
+			public List<PlaceOrigin> queryef(){
+				return zCommodityInfoService.queryef();
+			}
+			@GetMapping(value = "queryeg")
+			public List<Manufacturer> queryeg(){
+				return zCommodityInfoService.queryeg();
+			}
+			
+			
 			
 			@GetMapping
 			public List<CommodityInfo> DataQuery(){
@@ -25,6 +68,7 @@ public class ZCommodityInfoController {
 			public List<CommodityInfo> DataQueryFind(String cbno){
 				return zCommodityInfoService.DataQueryFind(cbno);
 			}
+			
 			
 			
 }
--- project/后台代码/dzwble/src/main/java/com/accp/service/ZCommodityInfoService.java
+++ project/后台代码/dzwble/src/main/java/com/accp/service/ZCommodityInfoService.java
@@ -6,14 +6,74 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.accp.domain.CommodityBrand;
 import com.accp.domain.CommodityInfo;
+import com.accp.domain.CommodityUnit;
+import com.accp.domain.CountryOrigin;
+import com.accp.domain.GoodsCategory;
+import com.accp.domain.GradeCommodity;
+import com.accp.domain.IncomeType;
+import com.accp.domain.Manufacturer;
+import com.accp.domain.PlaceOrigin;
+import com.accp.mapper.CommodityBrandMapper;
 import com.accp.mapper.CommodityInfoMapper;
+import com.accp.mapper.CommodityUnitMapper;
+import com.accp.mapper.CountryOriginMapper;
+import com.accp.mapper.GoodsCategoryMapper;
+import com.accp.mapper.GradeCommodityMapper;
+import com.accp.mapper.IncomeTypeMapper;
+import com.accp.mapper.ManufacturerMapper;
+import com.accp.mapper.PlaceOriginMapper;
 
 @Service
 @Transactional
 public class ZCommodityInfoService {
 			@Autowired
 			CommodityInfoMapper zCommodityInfoMapper;
+			
+			@Autowired
+			CommodityBrandMapper zcompp;   //商品品牌表
+			@Autowired
+			CommodityUnitMapper zcomdw;    //商品单位
+			@Autowired
+			GoodsCategoryMapper gooddalei; //商品大类
+			@Autowired
+			IncomeTypeMapper Income;      //收入类型
+			@Autowired
+			CountryOriginMapper count;    //原厂副厂
+			@Autowired
+			GradeCommodityMapper grade;  //商品等级
+			@Autowired
+			PlaceOriginMapper place;     //商品产地表
+			@Autowired
+			ManufacturerMapper manuf;    //厂商地址
+			
+			public List<CommodityBrand> query(){
+				return zcompp.selectByExample(null);
+			}
+			public List<CommodityUnit> querya(){
+				return zcomdw.selectByExample(null);
+			}
+			public List<GoodsCategory> queryab(){
+				return gooddalei.selectByExample(null);
+			}
+			public List<IncomeType> queryac(){
+				return Income.selectByExample(null);
+			}
+			public List<CountryOrigin> queryad(){
+				return count.selectByExample(null);
+			}
+			public List<GradeCommodity> querye(){
+				return grade.selectByExample(null);
+			}
+			public List<PlaceOrigin> queryef(){
+				return place.selectByExample(null);
+			}
+			public List<Manufacturer> queryeg(){
+				return manuf.selectByExample(null);
+			}
+			
+			
 			
 			/**
 			 * 连表查询Table
