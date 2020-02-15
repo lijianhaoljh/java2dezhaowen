0b435639ad02959e4367e00a07f850a460b8bb5d
pengyumei <iyumeipeng@163.com>
2020-02-15 21:28:44 +0800

    冲冲冲

--- project/后台代码/dzwble/src/main/java/com/accp/controller/DayController.java
+++ project/后台代码/dzwble/src/main/java/com/accp/controller/DayController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.accp.domain.Cardata;
+import com.accp.domain.Carkeep;
 import com.accp.domain.Customer;
+import com.accp.domain.Staff;
 import com.accp.service.DayService;
 
 @RestController
@@ -20,5 +23,50 @@ public class DayController {
 	@GetMapping("/querytodaybirthcus")
 	public List<Customer> querytodaybirthcus(){
 		return ser.querytodaybirthcus();
+	}
+
+	@GetMapping("/queryvipwillnone")
+	public List<Customer> queryvipwillnone(){
+		return ser.queryvipwillnone();
+	}
+
+	@GetMapping("/querylicensewillexpire")
+	public List<Cardata> querylicensewillexpire(){
+		return ser.querylicensewillexpire();
+	}
+
+	@GetMapping("/queryinsurexpirecar")
+	public List<Cardata> queryinsurexpirecar(){
+		return ser.queryinsurexpirecar();
+	}
+
+	@GetMapping("/querycheckcar")
+	public List<Cardata> querycheckcar(){
+		return ser.querycheckcar();
+	}
+
+	@GetMapping("/querymisscus")
+	public List<Customer> querymisscus(){
+		return ser.querymisscus();
+	}
+	
+	@GetMapping("/querytodatbirthdriver")
+	public List<Cardata> querytodatbirthdriver(){
+		return ser.querytodatbirthdriver();
+	}
+
+	@GetMapping("/querykeepcar")
+	public List<Carkeep> querykeepcar(){
+		return ser.querykeepcar();
+	}
+
+	@GetMapping("/querytodaybirthstaf")
+	public List<Staff> querytodaybirthstaf(){
+		return ser.querytodaybirthstaf();
+	}
+
+	@GetMapping("/queryoverstaf")
+	public List<Staff> queryoverstaf(){
+		return ser.queryoverstaf();
 	}
 }
