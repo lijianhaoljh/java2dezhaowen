6701fe84165542f9d353db411720fe2e87eb48bd
lijainhao-ljh <122572191@qq,com>
2020-02-15 21:30:32 +0800

    11111

---
+++ project/后台代码/dzwble/src/main/java/com/accp/domain/Requisition.java
@@ -0,0 +1,185 @@
+package com.accp.domain;
+
+import java.util.Date;
+
+public class Requisition {
+    private Integer id;
+
+    private String bid;
+
+    private Integer rtype;
+
+    private Date rtime;
+
+    private String pno;
+
+    private Integer cno;
+
+    private Integer num;
+
+    private Integer pcid;
+
+    private Float bpreferential;
+
+    private Float apreferential;
+
+    private Float bpreferentialm;
+
+    private String servicepeo;
+
+    private String free;
+
+    private String remark1;
+
+    private String remark2;
+
+    private String remark3;
+
+    private String remark4;
+
+    private String remark5;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public Integer getRtype() {
+        return rtype;
+    }
+
+    public void setRtype(Integer rtype) {
+        this.rtype = rtype;
+    }
+
+    public Date getRtime() {
+        return rtime;
+    }
+
+    public void setRtime(Date rtime) {
+        this.rtime = rtime;
+    }
+
+    public String getPno() {
+        return pno;
+    }
+
+    public void setPno(String pno) {
+        this.pno = pno;
+    }
+
+    public Integer getCno() {
+        return cno;
+    }
+
+    public void setCno(Integer cno) {
+        this.cno = cno;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public Integer getPcid() {
+        return pcid;
+    }
+
+    public void setPcid(Integer pcid) {
+        this.pcid = pcid;
+    }
+
+    public Float getBpreferential() {
+        return bpreferential;
+    }
+
+    public void setBpreferential(Float bpreferential) {
+        this.bpreferential = bpreferential;
+    }
+
+    public Float getApreferential() {
+        return apreferential;
+    }
+
+    public void setApreferential(Float apreferential) {
+        this.apreferential = apreferential;
+    }
+
+    public Float getBpreferentialm() {
+        return bpreferentialm;
+    }
+
+    public void setBpreferentialm(Float bpreferentialm) {
+        this.bpreferentialm = bpreferentialm;
+    }
+
+    public String getServicepeo() {
+        return servicepeo;
+    }
+
+    public void setServicepeo(String servicepeo) {
+        this.servicepeo = servicepeo;
+    }
+
+    public String getFree() {
+        return free;
+    }
+
+    public void setFree(String free) {
+        this.free = free;
+    }
+
+    public String getRemark1() {
+        return remark1;
+    }
+
+    public void setRemark1(String remark1) {
+        this.remark1 = remark1;
+    }
+
+    public String getRemark2() {
+        return remark2;
+    }
+
+    public void setRemark2(String remark2) {
+        this.remark2 = remark2;
+    }
+
+    public String getRemark3() {
+        return remark3;
+    }
+
+    public void setRemark3(String remark3) {
+        this.remark3 = remark3;
+    }
+
+    public String getRemark4() {
+        return remark4;
+    }
+
+    public void setRemark4(String remark4) {
+        this.remark4 = remark4;
+    }
+
+    public String getRemark5() {
+        return remark5;
+    }
+
+    public void setRemark5(String remark5) {
+        this.remark5 = remark5;
+    }
+}
--- project/后台代码/dzwble/src/main/java/com/accp/service/BillsService.java
+++ project/后台代码/dzwble/src/main/java/com/accp/service/BillsService.java
@@ -24,5 +24,26 @@ public class BillsService {
 		billsMapper.findBills();
 		return page.toPageInfo();
 	}
+	
+	//查询维修历史
+	public PageInfo<Bills> findByCdlicenseOne(Integer pageNum,Integer pageSize,String cdlicense){
+		Page<Bills> page=PageHelper.startPage(pageNum, pageSize);
+		billsMapper.findByCdlicenseOne(cdlicense);
+		return page.toPageInfo();
+	}
+	
+	//查训维修项目
+	public PageInfo<Bills> findByCdlicenseTwo(Integer pageNum,Integer pageSize,String cdlicense){
+		Page<Bills> page=PageHelper.startPage(pageNum, pageSize);
+		billsMapper.findByCdlicenseOne(cdlicense);
+		return page.toPageInfo();
+	}
+	
+	//查询领料情况
+	public PageInfo<Bills> findByCdlicenseThree(Integer pageNum,Integer pageSize,String cdlicense){
+		Page<Bills> page=PageHelper.startPage(pageNum, pageSize);
+		billsMapper.findByCdlicenseOne(cdlicense);
+		return page.toPageInfo();
+	}
 
 }
